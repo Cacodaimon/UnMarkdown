2e92ca753868a6e6a0a51af49a820038a2d6475c
Cacodaimon <mail@cacodaemon.de>
2020-10-03 19:23:33 +0200

    Matches any Unicode newline character \R instead of \n

--- src/MarkdownRemover.php
+++ src/MarkdownRemover.php
@@ -46,19 +46,19 @@ class MarkdownRemover
     {
         $this->replacements = [
             new UnMarkdownReplacement(
-                '/(?:^|\n)((?:.+\n)+)(?:\s{0,3})(?:(=+|-+))(?:\s*?)(?:\n|\Z)/',
+                '/(?:^|\R)((?:.+\R)+)(?:\s{0,3})(?:(=+|-+))(?:\s*?)(?:\R|\Z)/',
                 "\${1}\n",
                 'setext heading (Heading\n======, Heading\n------)',
                 UnMarkdownReplacement::TYPE_LEAF_BLOCK
             ),
             new UnMarkdownReplacement(
-                '/(?:^|\n)(?:\s{0,3})(-|\*|_)(?:( *\1){2,})(?:\s*?)(?:\n|\Z)/',
+                '/(?:^|\R)(?:\s{0,3})(-|\*|_)(?:( *\1){2,})(?:\s*?)(?:\R|\Z)/',
                 '',
                 'thematic break (hr) (---, ***, ___)',
                 UnMarkdownReplacement::TYPE_LEAF_BLOCK
             ),
             new UnMarkdownReplacement(
-                '/(?:\ *(?<=[^\\\\]|^)(`|~){3})(.*)\n(.*)(?:\s*?(?<=[^\\\\])\1{3})/Us',
+                '/(?:\ *(?<=[^\\\\]|^)(`|~){3})(.*)\R(.*)(?:\s*?(?<=[^\\\\])\1{3})/Us',
                 function ($matches) {
                     return $this->preserveCodeBlock($matches);
                 },
@@ -66,31 +66,31 @@ class MarkdownRemover
                 UnMarkdownReplacement::TYPE_LEAF_BLOCK
             ),
             new UnMarkdownReplacement(
-                '/(?:^|\n)\s{0,3}#{1,6}\s(.*)/',
+                '/(?:^|\R)\s{0,3}#{1,6}\s(.*)/',
                 "\n\${1}\n",
                 'heading with # (h1 to h6)',
                 UnMarkdownReplacement::TYPE_LEAF_BLOCK
             ),
             new UnMarkdownReplacement(
-                '/(?:^|\n)(\s*)(\*|\+|-)\s{1,3}\[x\]\s+((?!\2).+?)(?:\n|\Z)/i',
+                '/(?:^|\R)(\s*)(\*|\+|-)\s{1,3}\[x\]\s+((?!\2).+?)(?:\R|\Z)/i',
                 "\${1}$checkedTaskListPrefix\${3}",
                 'task list (checked) with *|+|-',
                 UnMarkdownReplacement::TYPE_CONTAINER_BLOCK
             ),
             new UnMarkdownReplacement(
-                '/(?:^|\n)(\s*)(\*|\+|-)\s{1,3}\[\s\]\s+((?!\2).+?)(?:\n|\Z)/i',
+                '/(?:^|\R)(\s*)(\*|\+|-)\s{1,3}\[\s\]\s+((?!\2).+?)(?:\R|\Z)/i',
                 "\${1}$unCheckedTaskListPrefix\${3}",
                 'task list (un checked) with *|+|-',
                 UnMarkdownReplacement::TYPE_CONTAINER_BLOCK
             ),
             new UnMarkdownReplacement(
-                '/(?:^|\n)(\s*)(\*|\+|-)(?:\s)((?!\2).+?)(?:\n|\Z)/',
+                '/(?:^|\R)(\s*)(\*|\+|-)(?:\s)((?!\2).+?)(?:\R|\Z)/',
                 "\${1}$unorderedListPrefix\${3}",
                 'list with *|+|-',
                 UnMarkdownReplacement::TYPE_CONTAINER_BLOCK
             ),
             new UnMarkdownReplacement(
-                '/(?:^|\n)\s{0,3}\>{1,}\s*(.*)/',
+                '/(?:^|\R)\s{0,3}\>{1,}\s*(.*)/',
                 "\n$quotePrefix\${1}",
                 'blockquotes',
                 UnMarkdownReplacement::TYPE_CONTAINER_BLOCK
